856dfacfae2abfd1cdfb75c745bb41078e650f33
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-03-23 22:42:32 +0530

    Default transition setup with HD image

--- src/ng-image-appear.js
+++ src/ng-image-appear.js
@@ -1,12 +1,30 @@
 "use strict";
+
+var el = document.getElementsByTagName('img'), imgElement;
+for(var i=0; i<el.length; i++) {
+    imgElement = el[i];
+    if(imgElement.hasAttribute('ng-image-appear')) {
+        imgElement.setAttribute('opacity', 0);
+    }
+}
+
 var ngImageAppear = angular.module('ngImageAppear', []).directive('ngImageAppear', function() {
     return {
         restrict: 'A',
         link: function(scope, element, attrs) {
+
+            // Fetching data attributes
+            var transitionDuration = attrs.transitionDuration;
+            console.log(transitionDuration);
+
+            element.css({
+                'opacity': 0
+            });
             element.bind('load', function() {
                 console.log("img loaded");
                 element.css({
-                    'display': 'block'
+                    'transition': ' all '+ transitionDuration+'s' +' ease-in-out ',
+                    'opacity': 1
                 });
             });
 
