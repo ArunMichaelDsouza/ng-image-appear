9ee86fd9e85c0080bdfca90197d51ade434891c5
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-03-25 01:24:19 +0530

    Foramatting code for core library

--- src/ng-image-appear.js
+++ src/ng-image-appear.js
@@ -1,63 +1,81 @@
 "use strict";
-
-// var el = document.getElementsByTagName('img'), imgElement;
-// for(var i=0; i<el.length; i++) {
-//     imgElement = el[i];
-//     if(imgElement.hasAttribute('ng-image-appear')) {
-//         imgElement.setAttribute('opacity', 0);
-//     }
-// }
-
 var ngImageAppear = angular.module('ngImageAppear', []).directive('ngImageAppear', function() {
     return {
-        priority: 1001,
         restrict: 'A',
         link: function(scope, element, attrs) {
 
+            // Hiding element from view during page load
             element.css({
                 'opacity': 0
             });
 
-            // Fetching data attributes
-            var transitionDuration = attrs.transitionDuration;
-            console.log(transitionDuration);
+            // Element attribute declarations
+            var transitionDuration, 
+                showLoader;
 
-            var showLoader = element[0].hasAttribute('show-loader');
+            // Fetching element attributes
+            transitionDuration = attrs.transitionDuration; // Get transition duration
+            console.log(transitionDuration);
+            showLoader = element[0].hasAttribute('show-loader'); // Check if loader is to be shown
             console.log(showLoader);
 
+            // Container for loader element
             var loaders = [];
 
-            if(showLoader) {
-                var elem = element[0];
-                var parent = elem.parentNode;
-                var wrapper = document.createElement('div');
-                wrapper.setAttribute('style', 'position: relative; display: inline-block; background-color: #eee;');
-                parent.replaceChild(wrapper, elem);
-                wrapper.appendChild(elem);
+            // DOM manipulation element declarations
+            var imgElement, 
+                parentElement, 
+                imgWrapper, 
+                loaderElement,
+                wrapperStyles = 'position: relative; display: inline-block; background-color: #eee;';
 
-                var loaderElement = document.createElement('img');
+            // Function to create image wrapper element
+            function generateImageWrapper() {
+                imgElement = element[0]; // Get image element
+                parentElement = imgElement.parentNode; // Find parent of image element
+
+                imgWrapper = document.createElement('div'); // Create wrapper element for image
+                imgWrapper.setAttribute('style', wrapperStyles); // Set default CSS for wrapper element
+
+                parentElement.replaceChild(imgWrapper, imgElement); // Replace actual image element with wrapper element
+                imgWrapper.appendChild(imgElement); // Append actual image element to wrapper element
+                // This will wrap the image element into a parent div tag used for showing the loader
+            };
+
+            // Function to render loader
+            function renderLoader() {
+                loaderElement = document.createElement('img'); 
                 loaderElement.src = 'http://www.fotos-lienzo.es/media/aw_searchautocomplete/default/loading.gif';
-                loaderElement.style = 'width: 5%; height: auto; position: absolute; left: 47.5%; top: 50%;';
+                loaderElement.style = 'width: 5%; height: auto; position: absolute; left: 47.5%; top: 45%;';
                 loaderElement.className = 'img-loader';
-                wrapper.appendChild(loaderElement);
+                imgWrapper.appendChild(loaderElement);
+            };
 
-                loaders = document.getElementsByClassName('img-loader');
+            // Show loader if attribute is present
+            if(showLoader) {
+                generateImageWrapper(); // Create image wrapper for loader
+                renderLoader(); // Show loader
+                loaders = document.getElementsByClassName('img-loader'); // Get loader element
             }
 
+            // Detect image load event
             element.bind('load', function() {
                 console.log("img loaded");
 
+                // Remove loader element if present
                 while (loaders[0]) {
                     loaders[0].parentNode.removeChild(loaders[0]);
+                    imgWrapper.style.backgroundColor = '';
+                    imgWrapper.style.position = '';
                 }
-               
-
+                
+                // Add CSS3 animation/transition to image element
                 element.css({
                     'transition': ' all '+ transitionDuration+'s' +' ease-in-out ',
-                    'opacity': 1
+                    'opacity': 1,
+                    'animation': 'fadeInUp .8s'
                 });
             });
-
         }
     };
 });
