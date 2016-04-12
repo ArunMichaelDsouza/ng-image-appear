20c0c10fad69c900b02ccf428d111a274d213ec5
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-04-13 00:43:15 +0530

    CSS3 powered animations added to build system

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,52 @@ module.exports = function(grunt) {
         uglify: {
             my_target: {
                 files: {
-                    'build/ng-image-appear.min.js': ['src/ng-image-appear.js']
+                    'dist/ng-image-appear.min.js': ['src/js/build/ng-image-appear.js']
+                }
+            }
+        },
+        cssmin: {
+            target: {
+                files: {
+                    'src/css/build/animations.min.css': ['src/css/animations.css']
                 }
             }
         },
         watch: {
             scripts: {
-                files: ['src/ng-image-appear.js'],
-                tasks: ['uglify'],
+                files: ['src/js/ng-image-appear.js'],
+                tasks: ['string-replace', 'uglify'],
                 options: {
                     spawn: false,
                 },
             },
+            styles: {
+                files: ['src/css/animations.css'],
+                tasks: ['cssmin', 'string-replace', 'uglify'],
+                options: {
+                    spawn: false,
+                },
+            },
+        },
+        'string-replace': {
+            inline: {
+                files: {
+                    'src/js/build/ng-image-appear.js': 'src/js/ng-image-appear.js',
+                },
+                options: {
+                    replacements: [
+                        {
+                            pattern: '@animation',
+                            replacement: '<%= grunt.file.read("src/css/build/animations.min.css") %>'
+                        }
+                    ]
+                }
+            }
         }
     });
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-string-replace');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.registerTask('default', ['watch']);
 };
