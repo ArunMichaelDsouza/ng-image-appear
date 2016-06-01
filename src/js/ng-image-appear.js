71a7bd20f8d7b9a202b85a0ef7d8efc27dca8424
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-06-01 17:30:32 +0530

    Build version update

--- src/js/ng-image-appear.js
+++ src/js/ng-image-appear.js
@@ -1,5 +1,5 @@
 /*
-    ng-image-appear v1.9.2
+    ng-image-appear v1.9.3
     Copyright (c) 2016 Arun Michael Dsouza (amdsouza92@gmail.com)
     Licence: MIT
     Demo on CodePen - http://codepen.io/amdsouza92/full/aNQeWW/
